1ccc76eaae9074353f6ab47752bde4700e52e04c
alal_t <kdergoj@gmail.com>
2021-12-03 06:17:39 +0300

    Update ora_pre.sql

--- ora_pre.sql
+++ ora_pre.sql
@@ -1,124 +1,230 @@
 
-
-
-CREATE TABLE TR_PRE
+-- Create table
+create table TR_PRE
 (
-  NNUM        NUMBER(17),
-  SCLOB       CLOB,
-  SSTR        VARCHAR2(4000 BYTE),
-  SUSER       VARCHAR2(2000 BYTE),
-  STRANS      VARCHAR2(4000 BYTE),
-  TTIMESTAMP  TIMESTAMP(6),
-  NVER        NUMBER(17),
-  SCOM0       VARCHAR2(2000 BYTE),
-  SCOM1       VARCHAR2(2000 BYTE),
-  SCOM2       VARCHAR2(2000 BYTE),
-  DDATE       DATE
+  nnum       NUMBER(17),
+  sclob      CLOB,
+  sstr       VARCHAR2(4000),
+  suser      VARCHAR2(2000),
+  strans     VARCHAR2(4000),
+  ttimestamp TIMESTAMP(6),
+  nver       NUMBER(17),
+  scom       VARCHAR2(2000),
+  scom2      VARCHAR2(2000),
+  scom3      VARCHAR2(2000),
+  ddate      DATE
 )
-nologing;
+
+nologging;
+-- Grant/Revoke object privileges 
+grant select on TR_PRE to PUBLIC;
+
 
 /
 
+create or replace package PRE is
 
-CREATE OR REPLACE package PRE is
-
-  function BT
-  /* Return format_error_stack + format_error_backtrace */
+function HAVING_CALL_S
+  (sWHO  in varchar2)
+  return varchar2;
+  
+  
+ function mr
+  /* many rows в строку */
+ (sTABLE         in varchar2,  --таблица или представление
+  sWHERE_COL     in varchar2,  --колонка идентификатор
+  nIDENT         in number default null,    -- идентификатор NUMBER
+  sIDENT         in varchar2 default null, -- идентификатор varchar
+  sRETURN_COL    in varchar2,  -- вернуть из колонки
+  nCNTROWS       in number default 3, -- кол-во строк макс.
+  sDELIM         in varchar2 default '; '
+  )
   return varchar2;
 
  procedure E
  /* Exception BOOLEAN  */
  (bVAL     in boolean,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   );
 
  procedure E
  /* Exception NUMBER  */
  (nVAL     in number,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   );
 
  procedure E
  /* Exception VARCHAR  */
  (sVAL     in varchar2,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   );
 
  procedure E
  /* Exception DATE */
  (dVAL     in date,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   );
 
  procedure S
  /* Сохранить NUMBER  */
  (nVAL     in number,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   );
 
  procedure S
  /* Сохранить VARCHAR */
  (sVAL     in varchar2,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   );
 
  procedure S
  /* Сохранить DATE  */
  (dVAL     in date,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   );
 
  procedure S
  /* Сохранить CLOB  */
  (cVAL     in clob,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   );
 
 
+ procedure SAC
+ /* Сохранить CHAR  */
+ (sVAL     in varchar2,
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
+  );
+
+ procedure SAC
+ /* Сохранить CLOB  */
+ (cVAL     in clob,
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
+  );
+
+  function BT
+  /* Return format_error_stack + format_error_backtrace */
+  return varchar2;
 
 end PRE;
 
 /
 
-CREATE OR REPLACE package body PRE is
+create or replace package body PRE is
 
 
-  function BT
-  /* Return format_error_stack + format_error_backtrace */
+function HAVING_CALL_S
+  (sWHO  in varchar2)
   return varchar2
   as
+  sRES varchar2(2000);
+  sINFO  varchar2(2000);
   BEGIN
-    return chr(10)||
-           'TRACE:'||chr(10)||
-           '==============================================================================================='||chr(10)||
-           (dbms_utility.format_error_stack||dbms_utility.format_error_backtrace)||chr(10)||
-           '===============================================================================================';
+    begin
+      SELECT 'HOST='||SYS_CONTEXT('USERENV','HOST')||'; IP='||SYS_CONTEXT('USERENV','IP_ADDRESS')||'; OSUSER='||SYS_CONTEXT('USERENV','OS_USER')
+      into   sINFO
+      FROM dual;
+      exception
+        when OTHERS then pre.sac(BT||'','ERROR IN CALL SYS_CONTEXT from HAVING_CALL_S');
+        sINFO := 'ERROR in SYS_CONTEXT';
+    end;
+    begin
+      SAC(sVAL => sWHO,
+          sCOM => 'CHECK_CALL_S',
+          sCOM2 => sINFO,
+          sCOM3 => '');
+      exception
+        when others then pre.sac(BT||'','ERROR IN CALL SAC from HAVING_CALL_S');
+    end;
+    /* return '$' for replace in calling object */
+    return '$';
   END;
+  
 
 
+ function mr
+  /* many rows в строку */
+ (sTABLE         in varchar2,  --таблица или представление
+  sWHERE_COL     in varchar2,  --колонка идентификатор
+  nIDENT         in number default null,    -- идентификатор NUMBER
+  sIDENT         in varchar2 default null, -- идентификатор varchar
+  sRETURN_COL    in varchar2,  -- вернуть из колонки
+  nCNTROWS       in number default 3, -- кол-во строк макс.
+  sDELIM         in varchar2 default '; '
+  )
+  return varchar2
+  as
+  sRES varchar2(2000);
+  --
+
+  /* Колонки*/
+  TYPE T_COL IS RECORD
+  (sTEXT varchar2(2000));
+  /* Массив */
+  --TYPE T_MS is TABLE of T_COL index by binary_integer;
+  TYPE T_MS is TABLE of varchar2(2000);
+
+  v_tbl T_MS;
+  --
+  sSQL varchar2(4000);
+  sCH  varchar2(10);
+
+  BEGIN
+    if nIDENT is null and sIDENT is null then
+      pre.E('Идентификаторы пустые');
+    end if;
+
+    sSQL := 'select to_char(t.'||sRETURN_COL||') from '||sTABLE||' t where t.'||sWHERE_COL||' = '||
+             case
+             when nIDENT is not null then nIDENT
+             when sIDENT is not null then sIDENT
+             end;
+
+    begin
+      execute immediate sSQL BULK COLLECT into v_tbl;
+    end;
+
+    if v_tbl.count() > 0 then
+      for c in v_tbl.first..v_tbl.last
+        loop
+          sRES := sRES ||sCH|| v_tbl(c);
+
+          if nCNTROWS = c then exit;
+          end if;
+
+          sCH := sDELIM;
+        end loop;
+    end if;
+
+    return ''||sRES;
+  END;
 
  procedure E
  /* Exception BOOLEAN  */
  (bVAL     in boolean,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   )
   as
   BEGIN
@@ -128,18 +234,6 @@ CREATE OR REPLACE package body PRE is
       when false then 'false'
       else 'else hz'
     end
-    ||case
-        when sCOM0 is null then (null)
-        else (chr(10)||' sCOM0 > '||sCOM0)
-      end
-    ||case
-        when sCOM1 is null then (null)
-        else (chr(10)||' sCOM1 > '||sCOM1)
-      end
-    ||case
-        when sCOM2 is null then (null)
-        else (chr(10)||' sCOM2 > '||sCOM2)
-      end
     );
   END;
 
@@ -147,76 +241,37 @@ CREATE OR REPLACE package body PRE is
  procedure E
  /* Exception NUMBER  */
  (nVAL     in number,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   )
   as
   BEGIN
-    raise_application_error(-20999,'n/'||chr(10)||to_char(nVAL)
-    ||case
-        when sCOM0 is null then (null)
-        else (chr(10)||' sCOM0 > '||sCOM0)
-      end
-    ||case
-        when sCOM1 is null then (null)
-        else (chr(10)||' sCOM1 > '||sCOM1)
-      end
-    ||case
-        when sCOM2 is null then (null)
-        else (chr(10)||' sCOM2 > '||sCOM2)
-      end
-      );
+    raise_application_error(-20999,'n/'||chr(10)||to_char(nVAL));
   END;
 
  procedure E
  /* Exception VARCHAR  */
  (sVAL     in varchar2,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   )
   as
   BEGIN
-    raise_application_error(-20999,'s/'||chr(10)||sVAL
-    ||case
-        when sCOM0 is null then (null)
-        else (chr(10)||' sCOM0 > '||sCOM0)
-      end
-    ||case
-        when sCOM1 is null then (null)
-        else (chr(10)||' sCOM1 > '||sCOM1)
-      end
-    ||case
-        when sCOM2 is null then (null)
-        else (chr(10)||' sCOM2 > '||sCOM2)
-      end
-      );
+    raise_application_error(-20999,'s/'||chr(10)||sVAL);
   END;
 
  procedure E
  /* Exception DATE */
  (dVAL     in date,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   )
   as
   BEGIN
-    raise_application_error(-20999,'d/'||chr(10)||to_char(dVAL,'dd.mm.yyyy')
-    ||case
-        when sCOM0 is null then (null)
-        else (chr(10)||' sCOM0 > '||sCOM0)
-      end
-    ||case
-        when sCOM1 is null then (null)
-        else (chr(10)||' sCOM1 > '||sCOM1)
-      end
-    ||case
-        when sCOM2 is null then (null)
-        else (chr(10)||' sCOM2 > '||sCOM2)
-      end
-      );
+    raise_application_error(-20999,'d/'||chr(10)||to_char(dVAL,'dd.mm.yyyy'));
   END;
 
 --=====================================================
@@ -225,12 +280,11 @@ CREATE OR REPLACE package body PRE is
  procedure S
  /* Сохранить NUMBER  */
  (nVAL     in number,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   )
  as
- pragma autonomous_transaction;
  v_nVER    number:=0;
  BEGIN
    begin
@@ -238,6 +292,7 @@ CREATE OR REPLACE package body PRE is
      into     v_nVER
      from     TR_PRE   t
      where    t.suser  = USER
+     and      t.strans = DBMS_TRANSACTION.LOCAL_TRANSACTION_ID
      group by t.nver;
      exception
        when NO_DATA_FOUND then begin
@@ -249,22 +304,20 @@ CREATE OR REPLACE package body PRE is
    end;
 
    begin
-     insert into TR_PRE(NNUM,SUSER,STRANS,TTIMESTAMP,NVER,SCOM0,SCOM1,SCOM2)
-     values            (nVAL,USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM0,sCOM1,sCOM2);
+     insert into TR_PRE(NNUM,SUSER,STRANS,TTIMESTAMP,NVER,SCOM,SCOM2,SCOM3)
+     values            (nVAL,USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM,sCOM2,sCOM3);
    end;
-   commit;
  END;
 
 
  procedure S
  /* Сохранить VARCHAR */
  (sVAL     in varchar2,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   )
  as
- pragma autonomous_transaction;
  v_nVER    number:=0;
  BEGIN
 
@@ -273,6 +326,7 @@ CREATE OR REPLACE package body PRE is
      into     v_nVER
      from     TR_PRE   t
      where    t.suser  = USER
+     and      t.strans = DBMS_TRANSACTION.LOCAL_TRANSACTION_ID
      group by t.nver;
      exception
        when NO_DATA_FOUND then begin
@@ -286,13 +340,13 @@ CREATE OR REPLACE package body PRE is
      case
        /* Если длинна строки больше 4000 - пишем в клоб  */
        when length(sVAL) > 4000 then
-         insert into TR_PRE(SCLOB,SSTR,SUSER,STRANS,TTIMESTAMP,NVER,SCOM0,SCOM1,SCOM2)
-         values            (sVAL,'TO CLOB',USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM0,sCOM1,sCOM2);
+         insert into TR_PRE(SCLOB,SSTR,SUSER,STRANS,TTIMESTAMP,NVER,SCOM,SCOM2,SCOM3)
+         values            (sVAL,'TO CLOB',USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM,sCOM2,sCOM3);
          commit;
        else
          /* Иначе строка */
-         insert into TR_PRE(SSTR,SUSER,STRANS,TTIMESTAMP,NVER,SCOM0,SCOM1,SCOM2)
-         values            (sVAL,USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM0,sCOM1,sCOM2);
+         insert into TR_PRE(SSTR,SUSER,STRANS,TTIMESTAMP,NVER,SCOM,SCOM2,SCOM3)
+         values            (sVAL,USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM,sCOM2,sCOM3);
          commit;
      end case;
    end;
@@ -301,12 +355,11 @@ CREATE OR REPLACE package body PRE is
  procedure S
  /* Сохранить DATE  */
  (dVAL     in date,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   )
  as
- pragma autonomous_transaction;
  v_nVER    number:=0;
  BEGIN
 
@@ -316,6 +369,7 @@ CREATE OR REPLACE package body PRE is
      into     v_nVER
      from     TR_PRE   t
      where    t.suser  = USER
+     and      t.strans = DBMS_TRANSACTION.LOCAL_TRANSACTION_ID
      group by t.nver;
      exception
        when NO_DATA_FOUND then begin
@@ -326,22 +380,20 @@ CREATE OR REPLACE package body PRE is
                                end;
    end;
    begin
-     insert into TR_PRE(DDATE,SUSER,STRANS,TTIMESTAMP,NVER,SCOM0,SCOM1,SCOM2)
-     values            (dVAL,USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM0,sCOM1,sCOM2);
+     insert into TR_PRE(DDATE,SUSER,STRANS,TTIMESTAMP,NVER,SCOM,SCOM2,SCOM3)
+     values            (dVAL,USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM,sCOM2,sCOM3);
    end;
-   commit;
  END;
 
 
  procedure S
  /* Сохранить CLOB  */
  (cVAL     in clob,
-  sCOM0     in varchar2 default null,
-  sCOM1    in varchar2 default null,
-  sCOM2    in varchar2 default null
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
   )
  as
- pragma autonomous_transaction;
  v_nVER    number:=0;
  BEGIN
 
@@ -351,6 +403,7 @@ CREATE OR REPLACE package body PRE is
      into     v_nVER
      from     TR_PRE   t
      where    t.suser  = USER
+     and      t.strans = DBMS_TRANSACTION.LOCAL_TRANSACTION_ID
      group by t.nver;
      exception
        when NO_DATA_FOUND then begin
@@ -361,14 +414,109 @@ CREATE OR REPLACE package body PRE is
                                end;
    end;
    begin
-     insert into TR_PRE(SCLOB,SSTR,SUSER,STRANS,TTIMESTAMP,NVER,SCOM0,SCOM1,SCOM2)
-     values            (cVAL,'IN CLOB',USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM0,sCOM1,sCOM2);
+     insert into TR_PRE(SCLOB,SSTR,SUSER,STRANS,TTIMESTAMP,NVER,SCOM,SCOM2,SCOM3)
+     values            (cVAL,'IN CLOB',USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM,sCOM2,sCOM3);
    end;
-   commit;
+ END;
+
+
+--=====================================================
+/* Сохраняем значение, с версией по транзакциям + автономный коммит COMMIT  */
+ procedure SAC
+ /* Сохранить VARCHAR */
+ (sVAL     in varchar2,
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
+  )
+ as
+ PRAGMA AUTONOMOUS_TRANSACTION;
+
+ v_nVER    number:=0;
+ BEGIN
+
+   begin
+     select   t.nver
+     into     v_nVER
+     from     TR_PRE   t
+     where    t.suser  = USER
+     and      t.strans = DBMS_TRANSACTION.LOCAL_TRANSACTION_ID
+     group by t.nver;
+     exception
+       when NO_DATA_FOUND then begin
+                                 select   nvl(max(t.nver),0)+1
+                                 into     v_nVER
+                                 from     TR_PRE   t
+                                 where    t.suser  = USER;
+                               end;
+   end;
+   begin
+     case
+       /* Если длинна строки больше 4000 - пишем в клоб  */
+       when length(sVAL) > 4000 then
+         insert into TR_PRE(SCLOB,SSTR,SUSER,STRANS,TTIMESTAMP,NVER,SCOM,SCOM2,SCOM3)
+         values            (sVAL,'TO CLOB',USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM,sCOM2,sCOM3);
+         commit;
+       else
+         /* Иначе строка */
+         insert into TR_PRE(SSTR,SUSER,STRANS,TTIMESTAMP,NVER,SCOM,SCOM2,SCOM3)
+         values            (sVAL,USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM,sCOM2,sCOM3);
+         commit;
+     end case;
+   end;
+ END;
+
+ procedure SAC
+ /* Сохранить CLOB + COMMIT  */
+ (cVAL     in clob,
+  sCOM     in varchar2 default null,
+  sCOM2    in varchar2 default null,
+  sCOM3    in varchar2 default null
+  )
+ as
+ /* !!!!!!!!!!!!!!!!!!!!!!!!!! */
+ PRAGMA AUTONOMOUS_TRANSACTION;
+
+ v_nVER    number:=0;
+ BEGIN
+
+
+   begin
+     select   t.nver
+     into     v_nVER
+     from     TR_PRE   t
+     where    t.suser  = USER
+     and      t.strans = DBMS_TRANSACTION.LOCAL_TRANSACTION_ID
+     group by t.nver;
+     exception
+       when NO_DATA_FOUND then begin
+                                 select   nvl(max(t.nver),0)+1
+                                 into     v_nVER
+                                 from     TR_PRE   t
+                                 where    t.suser  = USER;
+                               end;
+   end;
+
+   begin
+     insert into TR_PRE(SCLOB,SSTR,SUSER,STRANS,TTIMESTAMP,NVER,SCOM,SCOM2,SCOM3)
+     values            (cVAL,'IN CLOB',USER,DBMS_TRANSACTION.LOCAL_TRANSACTION_ID,CURRENT_TIMESTAMP,v_nVER,sCOM,sCOM2,sCOM3);
+     commit;
+   end;
+
  END;
 
 
 
-end PRE;
+--<<<<<<<<< MCRD SETTINGS <<<<<<<<<<<<<<
 
-/
+  function BT
+  /* Return format_error_stack + format_error_backtrace */
+  return varchar2
+  as
+  BEGIN
+    return chr(10)||
+           (dbms_utility.format_error_stack||dbms_utility.format_error_backtrace)||chr(10);
+  END;
+
+
+end PRE;
